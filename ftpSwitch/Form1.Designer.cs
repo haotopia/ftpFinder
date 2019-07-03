a6584114c7b691599519cac5d5e7a09a0a81a973
haotopia <haotopia@icloud.com>
2019-07-03 09:53:36 +0800

    基本功能实现

---
+++ ftpSwitch/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ftpSwitch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Easy = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Dictionary = new System.Windows.Forms.RadioButton();
+            this.StateShow = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 12F);
+            this.textBox1.Location = new System.Drawing.Point(299, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(508, 44);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "请输入IP地址";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 16F);
+            this.label1.Location = new System.Drawing.Point(161, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Start
+            // 
+            this.Start.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Start.Font = new System.Drawing.Font("宋体", 14F);
+            this.Start.Location = new System.Drawing.Point(169, 253);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(197, 58);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "开始扫描";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Font = new System.Drawing.Font("宋体", 14F);
+            this.Reset.Location = new System.Drawing.Point(399, 253);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(137, 58);
+            this.Reset.TabIndex = 3;
+            this.Reset.Text = "重置";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.StateShow);
+            this.panel1.Location = new System.Drawing.Point(166, 330);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(641, 356);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Easy
+            // 
+            this.Easy.AutoSize = true;
+            this.Easy.Font = new System.Drawing.Font("宋体", 14F);
+            this.Easy.Location = new System.Drawing.Point(299, 166);
+            this.Easy.Name = "Easy";
+            this.Easy.Size = new System.Drawing.Size(200, 42);
+            this.Easy.TabIndex = 0;
+            this.Easy.TabStop = true;
+            this.Easy.Text = "简单密码";
+            this.Easy.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 16F);
+            this.label2.Location = new System.Drawing.Point(161, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 43);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "模式";
+            this.label2.Click += new System.EventHandler(this.label2_Click_2);
+            // 
+            // Dictionary
+            // 
+            this.Dictionary.AutoSize = true;
+            this.Dictionary.Font = new System.Drawing.Font("宋体", 14F);
+            this.Dictionary.Location = new System.Drawing.Point(523, 166);
+            this.Dictionary.Name = "Dictionary";
+            this.Dictionary.Size = new System.Drawing.Size(162, 42);
+            this.Dictionary.TabIndex = 6;
+            this.Dictionary.TabStop = true;
+            this.Dictionary.Text = "字典序";
+            this.Dictionary.UseVisualStyleBackColor = true;
+            this.Dictionary.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // StateShow
+            // 
+            this.StateShow.Font = new System.Drawing.Font("幼圆", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.StateShow.Location = new System.Drawing.Point(0, 0);
+            this.StateShow.Name = "StateShow";
+            this.StateShow.Size = new System.Drawing.Size(639, 354);
+            this.StateShow.TabIndex = 0;
+            this.StateShow.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 872);
+            this.Controls.Add(this.Dictionary);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Easy);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "FTP扫描";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton Easy;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton Dictionary;
+        private System.Windows.Forms.RichTextBox StateShow;
+    }
+}
+
---
+++ ftpSwitch/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace ftpSwitch
+{
+    public partial class Form1 : Form
+    {
+        string ftpServerIP;
+        string[] ftpPassword = { "", "asd123", "111", "1", "aaa", " ", "123456" };
+        string FS;
+        bool trystate = true;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            ftpServerIP = textBox1.Text;
+
+            if (ValidateIPAddress(ftpServerIP))
+            {
+                ShowMsg("尝试匿名登陆");
+                FtpBySocket(ftpServerIP, "", "");
+                if (!trystate)
+                {
+                    foreach (string psw in ftpPassword)
+                    {
+                        FtpBySocket(ftpServerIP, "test", psw);
+                        if (trystate) break;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("IP输入格式不对哟，看看是不是输错了");
+            }
+
+            /*
+            if (ValidateIPAddress(ftpServerIP))
+            {
+                int i = 0;
+                StateShow.Text += "\r\n开始尝试匿名连接";
+                if (TryFtp(ftpServerIP, "", ""))
+                {
+                    StateShow.Text += "\r\n 匿名登陆成功";
+                    
+                }
+                else
+                {
+                    StateShow.Text += "\r\n 匿名登陆已关闭，尝试简单密码登陆";
+                }
+                while (!TryFtp(ftpServerIP, "test", ftpPassword[i])&&i<7)
+                {
+                    StateShow.Text += "\r\n 本次尝试-->用户名：test  ，密码：" + ftpPassword[i];
+                    if(i<6)
+                    i++;
+                }
+                if (i >= 7)
+                    StateShow.Text += "\r\n 简单密码尝试完毕，未找到匹配密码 " + i;
+                else
+                    StateShow.Text += "\r\n 扫描成功，用户名：test  密码：" + ftpPassword[i] + " \r\n";
+
+            }
+            else
+            {
+                MessageBox.Show("IP输入格式不对哟，看看是不是输错了");
+            }
+            */
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Easy.Checked = true;
+            StateShow.Text = "";
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public static bool ValidateIPAddress(string ipAddress)
+        {
+            Regex validipregex = new Regex(@"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
+            return (ipAddress != "" && validipregex.IsMatch(ipAddress.Trim())) ? true : false;
+        }
+
+        public bool TryFtp(string Ip, string User, string password)
+        {
+            bool state = true;
+            FtpWebRequest reqFtp;
+            reqFtp = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + ftpServerIP + "/"));
+            try
+            {
+
+                //测试ftp是否成功
+                reqFtp.Credentials = new NetworkCredential(User, password);
+                reqFtp.Method = WebRequestMethods.Ftp.ListDirectory;
+                reqFtp.KeepAlive = false;
+                reqFtp.Timeout = 1000;
+                FtpWebResponse response = (FtpWebResponse)reqFtp.GetResponse();
+                response.Close();
+            }
+            catch (Exception ex)
+            {
+                state = false;
+                StateShow.Text += "\r\n 发生错误 --> " + ex.Message;
+            }
+
+
+            return state;
+        }
+        Socket socket;
+        public int FtpBySocket(string Ip, string User, string Password, string Port = "21")
+        {
+
+            try
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Parse(ftpServerIP);
+                IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32("21"));
+                socket.Connect(point);
+                ShowMsg("端口开启");
+                Received();
+
+                socket.Send(Encoding.Default.GetBytes(string.Format("{0}{1}", "USER " + User, Environment.NewLine)));
+                Received();
+                socket.Send(Encoding.Default.GetBytes(string.Format("{0}{1}", "PASS " + Password, Environment.NewLine)));
+                Received();
+            }
+            catch (Exception ex)
+            {
+                ShowMsg("\r\n 发生错误 --> " + ex.Message);
+            }
+
+
+            return 0;
+        }
+
+        void ShowMsg(string str)
+        {
+            StateShow.AppendText(str + "\r\n");
+        }
+        void Received()
+        {
+
+
+            byte[] buffer = new byte[1024 * 1024 * 3];
+            //实际接收到的有效字节数
+            int len = socket.Receive(buffer);
+
+            string str = Encoding.UTF8.GetString(buffer, 0, len);
+            ShowMsg(socket.RemoteEndPoint + ":" + str);
+
+
+            if ((str.Contains("530") || str.Contains("501") || str.Contains("331")))
+            {
+                trystate = false;
+
+
+            }
+
+
+        }
+
+        void ChangeFS(string str)
+        {
+            FS = str;
+        }
+
+    }
+
+}
